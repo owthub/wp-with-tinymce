4d3fb79c9c7b91adc2c1bf4fcd2ba35ac4c0a8cf
Online Web Tutor <onlinewebtutorhub@gmail.com>
2018-12-12 05:33:53 +0530

    Setting options of addButton

--- editor.js
+++ editor.js
@@ -5,11 +5,19 @@ jQuery(function(){
        // adding custom buttons to tinymce editor
 
        editor.addButton("editor_console",{
-           text: "OWT Console"
+           //text: "OWT Console",
+           autofocus: false,
+           classes: "owt-custom-class",
+           disabled: false,
+           hidden: false,
+           image: "https://image.flaticon.com/icons/svg/733/733614.svg",
+           tooltip: "OWT Console Button"
+           //image: "<?php echo plugin_dir_url(__FILE__).''; ?>"
        });
 
        editor.addButton("editor_alert",{
-           text:"OWT alert"
+           text:"OWT alert",
+           tooltip: "OWT Alert Button"
        });
 
   });
